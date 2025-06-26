0f0d05a8161114e2ba493278b0569e2d5db4302f
888i88 <igor_palun@icloud.com>
2025-06-26 16:16:26 +0200

    replace tha callback to Promise syntax

--- 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
+++ 3-UsingAPIs/Week1/assignment/ex1-johnWho.js
@@ -8,21 +8,23 @@ Rewrite this function, but replace the callback syntax with the Promise syntax:
   didn't pass in a first name!"
 ------------------------------------------------------------------------------*/
 // TODO see above
-export const getAnonName = (firstName, callback) => {
-  setTimeout(() => {
-    if (!firstName) {
-      callback(new Error("You didn't pass in a first name!"));
-      return;
-    }
+export const getAnonName = (firstName) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (!firstName) {
+        callback(new Error("You didn't pass in a first name!"));
+        return;
+      }
 
-    const fullName = `${firstName} Doe`;
+      const fullName = `${firstName} Doe`;
 
-    callback(fullName);
-  }, 1000);
+      callback(fullName);
+    }, 1000);
+  });
 };
 
 function main() {
-  getAnonName('John', console.log);
+  getAnonName('John').then(console.log).catch(console.error);
 }
 
 // ! Do not change or remove the code below
